85914e23dd70c197085cab9a315083284169794f
PePito-M <59611653+PePito-M@users.noreply.github.com>
2020-06-18 18:46:11 +0100

    Update analysis.sh

--- analysis.sh
+++ analysis.sh
@@ -2,8 +2,10 @@
 # 1) change argument
 
 # 2)
-g++ main.cpp #compiling
-./a.out #running it
+#
+g++ -std=c++17 main_pedro.cpp          #compiling
+
+./a.out file_to_analyze set_tolerance  #running it
 
 # 3) rename file
 mv a.out JOHNPP_citr_all.out
