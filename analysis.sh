2b85cd8916f19bf020b579344fe75be6bc9a88be
PePito-M <59611653+PePito-M@users.noreply.github.com>
2020-07-01 19:13:07 +0100

    Update analysis.sh

--- analysis.sh
+++ analysis.sh
@@ -1,13 +1,6 @@
 
-# 1) change argument
+# 1) compiling
+g++ -std=c++17 main_pedro.cpp
+# 2) running it
+./a.out file_to_analyze set_tolerance(default=10)  #running it
 
-# 2)
-#
-g++ -std=c++17 main_pedro.cpp          #compiling
-
-./a.out file_to_analyze set_tolerance  #running it
-
-# 3) rename file
-mv a.out JOHNPP_citr_all.out
-
-# 4) run running.sh
